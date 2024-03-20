655e9da02ad52cf9e77e09d2d99dcedbf47c3afe
Sarahlaure <111229062+Sarahlaure@users.noreply.github.com>
2024-03-20 07:23:16 +0100

    Update DEVOIR 7_FOGWOUNG DJOUFACK SARAH-LAURE_ISEP2.R

--- DEVOIR 7_FOGWOUNG DJOUFACK SARAH-LAURE_ISEP2.R
+++ DEVOIR 7_FOGWOUNG DJOUFACK SARAH-LAURE_ISEP2.R
@@ -53,7 +53,7 @@ edit(cereales$classCereal_RizKg)
 
 ## Essayons de merger la base cereale avec la table de conversion 
 library(haven)
-table_de_conversion <- read_csv2(paste0("E:/ISEP 2/MON DOSSIER/SEMESTRE 2/TRAITEMENTS STATISTIQUES AVEC R/DOC EHCVM/Table_de_conversion_phase_2_1.xlsx"))
+table_de_conversion <- read_csv2(paste0("E:/ISEP 2/MON DOSSIER/SEMESTRE 2/TRAITEMENTS STATISTIQUES AVEC R/DOC EHCVM/Table_de_conversion_phase_2_1.csv"))
 View(table_de_conversion)
 
 ##-Supprimons les variables creees lors de l'importation
